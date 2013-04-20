f79f6014eb1bde9e5b44c334780c9526167eda1b
Ivan Shikhalev <shikhalev@gmail.com>
2013-04-21 00:41:59 +0600

    + yard-is-monkey: works

--- lib/yard-is-monkey.rb
+++ lib/yard-is-monkey.rb
@@ -8,6 +8,53 @@ module Is
 
     VERSION = '0.1.0'
 
+    class PropertyHandler < YARD::Handlers::Ruby::AttributeHandler
+
+      handles method_call(:property)
+      namespace_only
+
+      def process
+        return if statement.type == :var_ref || statement.type == :vcall
+        params = statement.parameters(false).dup
+        cls = nil
+        grp = 'Properties'
+        while params[-1].type == :list
+          arg = params.pop.source.scan(/(?::class\s*=>|class:)\s*([\w:]+)/)[0]
+          cls = arg[0] if arg
+        end
+        validated_attribute_names(params).each do |name|
+          namespace.attributes[scope][name] ||= SymbolHash[:read => nil,
+                                                           :write => nil]
+          { :read => name, :write => "#{name}=" }.each do |type, meth|
+            o = MethodObject.new namespace, meth, scope
+            case type
+            when :read
+              # par = [['*values']]
+              sgn = "def #{name} *values"
+              src = "#{sgn}\n  @#{name} << *values if ! values.empty? # abstract pseudocode\n  @#{name}\nend"
+            when :write
+              par = [['value']]
+              sgn = "def @{name}= value"
+              src = "#{sgn}\n  @#{name} = value # abstract pseudocode\nend"
+              doc = "Set new value to \"#{name}\" property."
+              if cls
+                doc += "\n@param [#{cls}] value"
+              end
+            end
+            o.parameters = par if par
+            o.signature = sgn
+            o.source = src
+            register(o)
+            o.docstring = doc if o.docstring.blank?(false)
+            o.docstring += "\n@return [#{cls}]" if cls
+            o.group = (scope == :class) ? "Class #{grp}" : "Instance #{grp}"
+            namespace.attributes[scope][name][type] = o
+          end
+        end
+      end
+
+    end
+
   end
 
 end
--- yard-is-monkey.gemspec
+++ yard-is-monkey.gemspec
@@ -4,4 +4,18 @@ require './lib/yard-is-monkey'
 
 Gem::Specification.new do |g|
   g.name = 'yard-is-monkey'
+  g.version = Is::MonkeyYARD::VERSION
+  g.summary = 'YARD plugin for use with is-monkey gem'
+  g.author = 'Ivan Shikhalev'
+  g.email = 'shikhalev@gmail.com'
+  g.description = g.summary + '.'
+  g.homepage = 'https://github.com/shikhalev/gems/'
+  g.license = 'GNU GPL'
+
+  g.files = [ 'lib/yard-is-monkey.rb', '.yardopts' ]
+  g.require_path = 'lib'
+  g.required_ruby_version = '>= 1.9.2'
+
+  g.add_dependency 'yard'
+  g.add_dependency 'is-monkey'
 end
